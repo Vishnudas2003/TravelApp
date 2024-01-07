1bce24718966edc7db7f38631611398bae93f7ea
vishnudas <vishnudas7412@gmail.com>
2024-01-07 12:06:10 -0500

    created the app fully

--- app/src/main/java/com/example/travell_app/MainActivity.kt
+++ app/src/main/java/com/example/travell_app/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.travell_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentImage = 1
+    lateinit var image : ImageView
+    var places = arrayOf("CN Tower", "High park", "Toronto Music Garden", "Berczy Park", "Bluffers Park")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val next = findViewById<ImageButton>(R.id.btnNext)
+        val previous = findViewById<ImageButton>(R.id.btnPrevious)
+        val placeName = findViewById<TextView>(R.id.tVName)
+        next.setOnClickListener{
+            var idCurrentImage= "place$currentImage"
+
+            var idCurrentImageInt= this.resources.getIdentifier(idCurrentImage, "id", packageName)
+            image= findViewById(idCurrentImageInt)
+            image.alpha = 0f
+
+            currentImage= (5+currentImage+1)%5
+            var idImageToShowString= "place$currentImage"
+            var idImageToShowInt= this.resources.getIdentifier(idImageToShowString, "id", packageName)
+            image = findViewById(idImageToShowInt)
+            image.alpha= 1f
+            placeName.text= places[currentImage]
+        }
+
+        previous.setOnClickListener{
+            var idCurrentImage= "place$currentImage"
+
+            var idCurrentImageInt= this.resources.getIdentifier(idCurrentImage, "id", packageName)
+            image= findViewById(idCurrentImageInt)
+            image.alpha = 0f
+
+            currentImage= (5+currentImage-1)%5
+            var idImageToShowString= "place$currentImage"
+            var idImageToShowInt= this.resources.getIdentifier(idImageToShowString, "id", packageName)
+            image = findViewById(idImageToShowInt)
+            image.alpha= 1f
+            placeName.text= places[currentImage]
+        }
     }
 }
